089494efc830b503ce8281a679b5a54f5f21aea7
gr7800 <gt29015@gmail.com>
2023-02-09 11:38:05 +0530

    json server completed

--- server.js
+++ server.js
@@ -10,3 +10,5 @@ server.use(router);
 server.listen(port,()=>(
     console.log("Json server is running on localhost http://localhost:3000/")
 ));
+
+// https://mock3-jsonserver.onrender.com/users/?id=1&id=2&_sort=age&_order=desc
